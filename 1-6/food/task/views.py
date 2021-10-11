2c0b0385f7a2a56c6ebde2fad7281593d82dbe5f
nilna01 <nilnasyariifah01@gmail.com>
2021-10-11 16:48:16 +0700

    tabel

---
+++ 1-6/food/task/views.py
@@ -0,0 +1,22 @@
+from django.http import request
+from django.shortcuts import redirect, render
+from . import models
+
+# Create your views here.
+def index (request) :
+    if request.POST :
+        models.Task.objects.create(item=request.POST['item'])
+        return redirect('/')
+
+    tasks = models.Task.objects.all()
+    return render(request, 'index.html', {
+    'data' : tasks,
+    }) 
+def delete(request, id) :
+    models.Task.objects.filter(pk=id).delete()
+    return redirect('/')
+
+def rincian(request, id) :
+    models.Task.objects.filter(pk=id).first()
+    return redirect('/')        
+
