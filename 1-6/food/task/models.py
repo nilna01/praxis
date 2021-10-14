b0d87f265c802b0f18effafc453c488584217b26
nilna01 <nilnasyariifah01@gmail.com>
2021-10-14 16:46:01 +0700

    food

--- 1-6/food/task/models.py
+++ 1-6/food/task/models.py
@@ -3,6 +3,8 @@ from django.db.models.base import Model
 
 # Create your models here.
 class Task(models.Model):
-    makanan = models.TextField(max_length= 200)
-    harga = models.IntegerField()
-    jumlah = models.IntegerField()
+    makanan = models.TextField(default='', max_length= 200)
+    minuman = models.TextField(default='' ,max_length= 200)
+    harga = models.IntegerField(default='')
+    jumlah = models.IntegerField(default='')
+
--- 1-6/food/task/views.py
+++ 1-6/food/task/views.py
@@ -26,6 +26,7 @@ def tambah(request):
     if request.POST:
         models.Task.objects.create(
             makanan=request.POST['makanan'],
+            minuman=request.POST['minuman'],
             harga=request.POST['harga'],
             jumlah=request.POST['jumlah'])
 
@@ -38,6 +39,7 @@ def update (request,id):
     if request.POST:
         models.Task.objects.filter(pk=id). update(
             makanan=request.POST['makanan'],
+            minuman=request.POST['minuman'],
             harga=request.POST['harga'],
             jumlah=request.POST['jumlah'])
             
